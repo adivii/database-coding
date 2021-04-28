5c2df19994c9e6650d7ea3b13f5d42831803f240
mralifakbar <alif.akbarkartadinata2049@students.unila.ac.id>
2021-04-28 13:28:16 +0700

    Update hello.cpp

--- C++/Hello World/hello.cpp
+++ C++/Hello World/hello.cpp
@@ -6,7 +6,9 @@ using namespace std;
 //fungsi main()
 int main(){
 	int usia;
+	cout<<"Selamat Pagi" << endl;
     cout<<"Usia Anda : ";cin>>usia;
     cout<<"Usia Anda "<<usia<<" Tahun"<<endl;
+    cout << "baris alip" << endl;
     return 0;
 }
